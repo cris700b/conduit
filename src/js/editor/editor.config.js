4e9cc25f9b922d69659f629218ac881551775276
Cristian Birzu <cristian.birzu@gmail.com>
2016-07-04 19:02:04 +0200

    added article editor functionality
    the editor template has also been refactored to take advantage of the
    new functionality of the form-control mixin for handling fieldset
    attributes and field attributes

--- src/js/editor/editor.config.js
+++ src/js/editor/editor.config.js
@@ -5,7 +5,7 @@ function EditorConfig($stateProvider){
 
     $stateProvider.state('app.editor', {
 
-        url: '/editor',
+        url: '/editor/:slug',
         controller: 'EditorCtrl',
         controllerAs: '$ctrl',
         templateUrl: 'editor/editor.html',
@@ -15,6 +15,41 @@ function EditorConfig($stateProvider){
             auth: function(User){
 
                 return User.ensureIsAuth(true);
+            },
+            article: function($state, $stateParams, User, Articles){
+
+                // if we're trying to edit an article
+                if($stateParams.slug){
+
+                    return Articles.get($stateParams.slug)
+                                    .then((article) => {
+
+                                        // if the current user is the author,
+                                        // resolve the article data
+                                        if(User.current.username === article.author.username){
+
+                                            return article;
+                                        }
+
+                                        // otherwise, redirect them to the home page
+                                        else{
+
+                                            $state.go('app.home');
+                                        }
+                                    },
+
+                                    // if there is an error (article does not exist), redirect to the home page
+                                    (err) => {
+
+                                        $state.go('app.home')
+                                    })
+                }
+
+                // if thise is a new article, then just return null
+                else{
+
+                    return null;
+                }
             }
         }
     })
--- src/js/editor/editor.controller.js
+++ src/js/editor/editor.controller.js
@@ -1,12 +1,60 @@
 
 class EditorCtrl{
 
-    constructor(){
+    constructor($state, Articles, article){
 
         'ngInject';
 
+        this._state = $state;
+        this._Articles = Articles;
+
+        if(!article){
+
+            this.article= {
+
+                title: '',
+                description: '',
+                body: '',
+                tagList: ['first targ', 'seccond tag']
+
+            };
+        }
+        else{
+
+            this.article = article;
+        }
+    }
+
+    addTag(){
+
+        // control that the tag is not already in the array
+        if(!this.article.tagList.includes(this.tagField)){
+
+            this.article.tagList.push(this.tagField);
+            this.tagField = '';
+        }
+    }
+
+    removeTag(tagName){
+
+        this.article.tagList = this.article.tagList.filter((slug) => slug != tagName);
+    }
+
+    submit(){
+
+        this.isSubmitting = true;
+        this._Articles.save(this.article)
+                        .then((newArticle) => {
+
+                            this._state.go('app.article', {slug: newArticle.slug})
+                        },
+                        (err) => {
+
+                            this.isSubmitting = false;
+                            this.errors = err.data.errors;
+                        });
 
     }
 }
 
-export default EditorCtrl
+export default EditorCtrl;
