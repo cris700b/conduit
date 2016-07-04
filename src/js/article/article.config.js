933a4f32d6817ce97c5b1ff9880ce61bdc45dfa9
Cristian Birzu <cristian.birzu@gmail.com>
2016-07-04 19:03:05 +0200

    added article functionality 2

--- src/js/article/article.config.js
+++ src/js/article/article.config.js
@@ -7,7 +7,22 @@ function ArticleConfig($stateProvider) {
     controller: 'ArticleCtrl',
     controllerAs: '$ctrl',
     templateUrl: 'article/article.html',
-    title: 'Article'
+    title: 'Article',
+
+    // when the controller loads,
+    // the title of the web page will be changed to the article's title
+    resolve: {
+
+        article: function($state, $stateParams, Articles){
+
+            return Articles.get($stateParams.slug)
+                            .then(
+                                (article) => article,
+                                (err) => $state.go('app.home')
+                            );
+
+        }
+    }
   });
 
 };
