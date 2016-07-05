70161b2af62592bbda930c585c96362c5986d6c2
Cristian Birzu <cristian.birzu@gmail.com>
2016-07-05 18:16:10 +0200

    added article-meta component

---
+++ src/js/components/article-helpers/article-meta.component.js
@@ -0,0 +1,12 @@
+
+let ArticleMeta = {
+
+    bindings: {
+
+        article: '='
+    },
+    transclude: true,
+    templateUrl: 'components/article-helpers/article-meta.html'
+};
+
+export default ArticleMeta;
--- src/js/components/index.js
+++ src/js/components/index.js
@@ -9,11 +9,15 @@ import ShowAuthed from './show-authed.directive';
 // import the follow-btn component
 import FollowBtn from './buttons/follow-btn.component';
 
+// import the article-meta component
+import ArticleMeta from './article-helpers/article-meta.component';
+
 let componentsModule = angular.module('app.components', []);
 
 // the list of available components
 componentsModule.component('listErrors', ListErrors)
-                .component('followBtn', FollowBtn);
+                .component('followBtn', FollowBtn)
+                .component('articleMeta', ArticleMeta);
 
 // the list of available directives
 componentsModule.directive('showAuthed', ShowAuthed);
